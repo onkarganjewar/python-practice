b9bc424950600406351b4540b63f0e13492be3ac
Onkar Ganjewar <ganjewaronkar@gmail.com>
2017-03-09 21:32:53 -0800

    Simple login/add session
    Added a session attribute to store the logged-in-user-data and added
    default logout handlers.

--- simple login module using flask/app.py
+++ simple login module using flask/app.py
@@ -1,5 +1,5 @@
 # import the Flask class from the flask module
-from flask import Flask, render_template, redirect, url_for, request
+from flask import Flask, render_template, redirect, url_for, request, session
 
 # create the application object
 app = Flask(__name__)
@@ -13,7 +13,13 @@ def home():
 
 @app.route('/index')
 def index():
-    return render_template('index.html')  # render index template
+    if 'username' in session:
+        username = session['username']
+        return render_template('index.html', username=username)
+#        return 'Logged in as ' + username + '<br>' + \
+#            "<b><a href = '/logout'>click here to log out</a></b>"
+    return "You are not logged in <br><a href = '/login'></b>" + \
+        "click here to log in</b></a>"
 
 
 @app.route('/welcome')
@@ -30,10 +36,20 @@ def login():
                 request.form['password'] != 'admin'):
             error = 'Invalid Credentials. Please try again.'
         else:
+            session['loggedIn'] = True
+            session['username'] = request.form['username']
             return redirect(url_for('index'))
     return render_template('login.html', error=error)
 
 
+# route for handling the login page logic
+@app.route('/logout')
+def logout():
+    session.pop('loggedIn', None)
+    return redirect(url_for('login'))
+
+
 # start the server with the 'run()' method
 if __name__ == '__main__':
+    app.secret_key = 'super secret key'
     app.run(debug=True)
