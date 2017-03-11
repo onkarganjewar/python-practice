4aaca3fb7bdfa86eb93a2315adc09c3a7ab27566
Onkar Ganjewar <ganjewaronkar@gmail.com>
2017-03-10 20:48:21 -0800

    Simple login/add user.py & modify dbTest.py
    Added a sample User class. Also, tested a sample db insert operation in
    an existing table using custom User class.

--- simple login module using flask/dbTest.py
+++ simple login module using flask/dbTest.py
@@ -1,4 +1,6 @@
 import pymysql
+from user import User
+
 
 '''
 class User(db.Model):
@@ -19,9 +21,9 @@ class User(db.Model):
 
 def dbConnect():
     # db = pymysql.connect(host='10.0.0.140', user='root', passwd='password')
-    db = pymysql.connect(host='127.0.0.1', user='root')
+    db = pymysql.connect(host='127.0.0.1', user='root', db='demo')
     cursor = db.cursor()
-    query = ("SHOW DATABASES")
+    query = ("SHOW TABLES")
     cursor.execute(query)
     # Print the list of databases in mysql localhost
     for r in cursor:
@@ -30,12 +32,19 @@ def dbConnect():
 
 
 # Insert to the database
-def dbInsert(db):
+def dbInsert(db, User):
+    firstName = User.firstName
+    lastName = User.lastName
+    email = User.email
+    password = User.password
+
     try:
         with db.cursor() as cursor:
             # Create a new record
-            sql = "INSERT INTO `users` (`email`, `password`) VALUES (%s, %s)"
-            cursor.execute(sql, ('webmaster@python.org', 'very-secret'))
+            sql = "INSERT INTO `users` (`FirstName`, `LastName`, `Password`, \
+                   `Email`) VALUES (%s, %s, %s, %s)"
+            cursor.execute(sql, (firstName, lastName, password, email))
+            # cursor.execute(sql, ('webmaster@python.org', 'very-secret'))
 
         # connection is not autocommit by default. So you must commit to save
         # your changes.
@@ -43,13 +52,18 @@ def dbInsert(db):
 
         with db.cursor() as cursor:
             # Read a single record
-            sql = "SELECT `id`, `password` FROM `users` WHERE `email`=%s"
+            sql = "SELECT `UserId`, `password`, `FirstName`, `LastName` \
+            FROM `users` WHERE `email`=%s"
             cursor.execute(sql, ('webmaster@python.org',))
-            result = cursor.fetchone()
-            print(result)
+            # result = cursor.fetchone()
+            result = cursor.fetchall()
+            for record in result:
+                print(record)
     finally:
         db.close()
 
 
 connection = dbConnect()
-dbInsert(connection)
+customUser = User("onkar", "ganjewar", "webmaster@python.org",
+                  "password")
+dbInsert(connection, customUser)
---
+++ simple login module using flask/user.py
@@ -0,0 +1,74 @@
+# circular dependency
+# from dbTest import dbConnect
+from werkzeug.security import generate_password_hash, check_password_hash
+
+# db = dbConnect()
+
+
+class User(object):
+    firstName = ""
+    email = ""
+    # primary_key AUTO_INCREMENT
+    # id = None
+    password = ""
+    lastName = ""
+
+    def __init__(self, firstName, lastName, email, password):
+        self.firstName = firstName
+        self.lastName = lastName
+        self.email = email
+        self.set_password(password)
+
+    def set_password(self, password):
+        self.password = generate_password_hash(password)
+
+    def check_password(self, password):
+        return check_password_hash(self.password, password)
+
+    def __repr__(self):
+        return '<User %r>' % (self.email)
+
+
+'''
+    def get_id(self):
+        return str(self.id)
+'''
+
+
+'''
+class User(db.Model):
+    __tablename__ = "users"
+    id = db.Column('user_id', db.Integer, primary_key=True)
+    username = db.Column('username', db.String(20), unique=True, index=True)
+    password = db.Column('password', db.String(250))
+    email = db.Column('email', db.String(50), unique=True, index=True)
+    registered_on = db.Column('registered_on', db.DateTime)
+    todos = db.relationship('Todo', backref='user', lazy='dynamic')
+
+    def __init__(self, username, password, email):
+        self.username = username
+        self.set_password(password)
+        self.email = email
+        self.registered_on = datetime.utcnow()
+
+    def set_password(self, password):
+        self.password = generate_password_hash(password)
+
+    def check_password(self, password):
+        return check_password_hash(self.password, password)
+
+    def is_authenticated(self):
+        return True
+
+    def is_active(self):
+        return True
+
+    def is_anonymous(self):
+        return False
+
+    def get_id(self):
+        return str(self.id)
+
+    def __repr__(self):
+        return '<User %r>' % (self.username)
+'''
