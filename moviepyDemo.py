dba3529b1965bd5bf987a7077d90f8c5555d3495
Onkar Ganjewar <ganjewaronkar@gmail.com>
2017-04-04 12:05:49 -0700

    Add sample moviepyDemo script
    Added a sample python3 script that takes a video clip as an input and writes the transformed vertically flipped video as a file. Used moviepy package for video clip transformations.

---
+++ moviepyDemo.py
@@ -0,0 +1,42 @@
+# Video clip transformations using moviepy
+# noqa: E402
+
+import imageio
+imageio.plugins.ffmpeg.download()
+from moviepy.editor import VideoFileClip
+import cv2
+import utils
+import time
+import datetime
+
+
+def flip(image):
+    """Flips an image vertically """
+    return image[::-1]  # remember that image is a numpy array
+
+
+clip = VideoFileClip("test_video.mp4")
+
+# timer = Timer()
+# timer.tic()
+start = time.time()
+# Transform video and perform image flip
+new_clip = clip.fl_image(flip)
+end = time.time()
+total_time = (end - start)
+
+# timer.toc()
+# print( datetime.timedelta(seconds=clip.duration))
+
+clip_len = time.strftime("%H:%M:%S", time.gmtime(clip.duration))
+print((('Image flips took {:.3f}s for '
+        '{} long video').format(total_time, clip_len)))
+
+
+# Saves the frame at 1.2 sec
+# clip1.save_frame("D:\\SJSU\\295B\\Udacity\\CarND-LaneLines-P1-Submit\\
+#                    video-frames\\frames1_2.jpeg", t=1.2) # Windows file path
+
+
+# Write a video to a file
+new_clip.write_videofile("test_output.mp4", audio=False)
